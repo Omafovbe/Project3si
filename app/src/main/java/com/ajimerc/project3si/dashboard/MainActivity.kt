ca65136df197684925212730fae9a87645ecc6e0
Omafovbe <i.omafovbe@gmail.com>
2020-09-26 09:23:29 +0100

    Initial commit

---
+++ app/src/main/java/com/ajimerc/project3si/dashboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ajimerc.project3si
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ajimerc/project3si/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.ajimerc.project3si.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
